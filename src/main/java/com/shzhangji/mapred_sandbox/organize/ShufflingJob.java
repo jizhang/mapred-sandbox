255506b77f01b95ab0f793e4b82d0e2a1e35c33a
Ji Zhang <jizhang@anjuke.com>
2014-01-21 22:33:39 +0800

    shuffling

--- src/main/java/com/shzhangji/mapred_sandbox/organize/ShufflingJob.java
+++ src/main/java/com/shzhangji/mapred_sandbox/organize/ShufflingJob.java
@@ -1,6 +1,7 @@
 package com.shzhangji.mapred_sandbox.organize;
 
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -10,14 +11,13 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class BinningJob extends Configured implements Tool {
+public class ShufflingJob extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
@@ -28,57 +28,56 @@ public class BinningJob extends Configured implements Tool {
         FileSystem.get(getConf()).delete(output, true);
 
         Job job = new Job(getConf());
-        job.setJarByClass(BinningJob.class);
+        job.setJarByClass(ShufflingJob.class);
 
         FileInputFormat.addInputPath(job, input);
         FileOutputFormat.setOutputPath(job, output);
 
         job.setMapperClass(JobMapper.class);
-        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(Text.class);
-        job.setNumReduceTasks(0);
 
-        MultipleOutputs.addNamedOutput(job, "bins", TextOutputFormat.class, NullWritable.class, Text.class);
+        job.setReducerClass(JobReducer.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
-    public static class JobMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+    public static class JobMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
 
-        private MultipleOutputs<NullWritable, Text> mos;
+        private Random random;
 
         @Override
         protected void setup(Context context) throws IOException,
                 InterruptedException {
 
-            mos = new MultipleOutputs<NullWritable, Text>(context);
+            random = new Random();
         }
 
         @Override
         protected void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
 
-            String[] segs = value.toString().split("\\s+");
-
-            if (segs[0].equals("hadoop")) {
-                mos.write("bins", NullWritable.get(), value, "hadoop");
-            } else if (segs[0].equals("hive")) {
-                mos.write("bins", NullWritable.get(), value, "hive");
-            } else {
-                mos.write("bins", NullWritable.get(), value, "unknown");
-            }
+            context.write(new LongWritable(random.nextLong()), value);
         }
 
-        @Override
-        protected void cleanup(Context context) throws IOException,
-                InterruptedException {
+    }
 
-            mos.close();
+    public static class JobReducer extends Reducer<LongWritable, Text, NullWritable, Text> {
+
+        @Override
+        protected void reduce(LongWritable key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+
+            for (Text value : values) {
+                context.write(NullWritable.get(), value);
+            }
         }
 
     }
 
     public static void main(String[] args) throws Exception {
-        System.exit(ToolRunner.run(new BinningJob(), args));
+        System.exit(ToolRunner.run(new ShufflingJob(), args));
     }
 }
